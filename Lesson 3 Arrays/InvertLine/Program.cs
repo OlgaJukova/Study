f5e1ab83e5239801e9ddaacde6c643b2b7fb8f71
OlgaJukova <OlgaJukova3787@yandex.ru>
2021-02-23 22:24:28 +0300

    Compliting tasks for lesson 3. Project SeaBatle isnt fully completed

---
+++ Lesson 3 Arrays/InvertLine/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvertLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String line = "";
+
+            while (line.Length == 0)
+            {
+                Console.WriteLine("Ведите строку");
+                line = Console.ReadLine();
+
+                if (line.Length > 0)
+
+                {
+                    for (int j = line.Length - 1; j >= 0; j--)
+                    {
+                        Console.Write(line[j]);
+
+                    }
+                    Console.ReadLine();
+                }
+            }
+            
+        }
+    }
+}
---
+++ Lesson 3 Arrays/Shift/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = { 51, 0, 8, 90, -7, 56, 5, 7};
+            int n =-10 ,
+                n1 = 0,// счетчик количества итерраций сдвига массива на 1
+                n2 = 0;// вспомогательная переменная для пересмотра количества сдвигов, если n превышает длину массива
+
+            Console.Write("Исходный массив:     ");
+            for (int z = 0; z < mas.Length; z++)
+            {
+                Console.Write(mas[z] + " ");
+            };
+
+                       
+            
+           if (n >= 0)
+            {
+
+                if (n > mas.Length)  { n2 = n - mas.Length; }   else { n2 = n; }; // уменьшаем количество итерраций, если n больше длины массива
+
+                n1 = 1; // счетчик количества итерраций сдвига массива на 1
+                int x = 0;
+                while (n1 <= n2)
+                {
+                     x = mas[mas.Length - 1]; //запоминаем последний элемент массива
+                    for (int i = (mas.Length-1); i >0 ; i--)
+                    {
+                        mas[i] = mas[i - 1];
+
+
+                    };
+                    n1++;
+                    mas[0] = x;
+                };
+                
+
+                Console.WriteLine("\n");
+                Console.Write("Со cдвигом на "+n+":      ");
+
+                for (int j=0; j < mas.Length; j++)
+                {
+                    Console.Write(mas[j]+" ");
+                };
+                Console.ReadLine();
+            };
+
+
+            if (n < 0)
+            {
+                int nAbs = Math.Abs(n); // вычисляем абсолютное число сдвигов влево
+                if (nAbs > mas.Length) { n2 = nAbs - mas.Length; } else { n2 = nAbs; }; // уменьшаем количество итерраций, если n больше длины массива
+
+                n1 = 1; // счетчик количества итерраций сдвига массива на 1
+                int x = 0;
+                while (n1 <= n2)
+                {
+                    x = mas[0]; //запоминаем первый элемент массива
+                    for (int i =0 ; i < (mas.Length - 1) ; i++)
+                    {
+                        mas[i] = mas[i + 1];
+
+
+                    };
+                    n1++;
+                    mas[(mas.Length - 1)] = x;
+                };
+
+
+                Console.WriteLine("\n");
+                Console.Write("Со cдвигом на " + n + ":      ");
+
+                for (int j = 0; j < mas.Length; j++)
+                {
+                    Console.Write(mas[j] + " ");
+                };
+                Console.ReadLine();
+            };
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
