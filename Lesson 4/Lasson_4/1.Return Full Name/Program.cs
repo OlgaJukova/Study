87aa238300c0345d9422981d1d3feeb37032ee29
OlgaJukova <OlgaJukova3787@yandex.ru>
2021-02-28 18:07:58 +0300

    Exercises for lesson 4

---
+++ Lesson 4/Lasson_4/1.Return Full Name/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Return_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] names = { { "Петров", "Федор","Иванович"},
+                                { "Лопухина", "Зинаида","Ильинична"},
+                                { "Проничкин", "Дмитрий","Юрьевич"},
+            };
+
+            /*for (int i = 0; i < names.GetLength(0); i++)
+            {
+
+                string FullName = GetFullName(names[i, 0], names[i, 1], names[i, 2]);
+                Console.WriteLine(FullName);
+                
+            };
+
+            Console.ReadKey();
+        }
+
+
+        static string GetFullName(string firstname, string secondname, string patronymic )
+        {
+            return firstname +" "+ secondname +" "+ patronymic;
+        } */
+
+
+
+            for (int i = 0; i < names.GetLength(0); i++)
+            {
+                string FullName1 = GetFullName(names[i, 0], names[i, 1], names[i, 2]);
+                Console.WriteLine(FullName1);
+            }
+
+            Console.ReadKey();
+
+        }
+
+        static string GetFullName(params string[] str)
+        {
+            string FullName = "";
+            for (int j = 0; j < str.Length; j++)
+            {
+                FullName += str[j]+" ";
+            }
+            return FullName;
+        } 
+    }
+}
---
+++ Lesson 4/Lasson_4/Fibonacci/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            int n1 = 0, n2 = 1, n3 = 1, i = 1, n = 15;
+                string str = "";
+
+            (n1, n2, n3, n, i, str) = Fibonacci(0, 1, 1, 15, 1, str);
+
+            Console.WriteLine(str);
+            Console.ReadKey();
+
+        }
+
+
+
+        static (int n1, int n2, int n3,int n,int i, string str) Fibonacci( int n1, int n2, int n3, int n, int i,string str)
+        {
+            if (i <= n)
+            {
+                n3 = n2 + n1;
+                str += (" " + Convert.ToString(n3));
+                n1 = n2;
+                n2 = n3;
+                i++;
+                return (n1, n2, n3, n,i, str) = Fibonacci(n1, n2, n3, n,i, str);
+            } else
+            {
+               return  Fibonacci(n1, n2, n3, n, i, str);
+            }
+
+
+        }
+    
+    
+    
+    
+    }
+}
