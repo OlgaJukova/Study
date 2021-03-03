d3e22eab02040f4a2c288c2a6bb8cd575fca27ef
OlgaJukova <OlgaJukova3787@yandex.ru>
2021-03-03 17:53:33 +0300

    Exersize 5. File sistem/

---
+++ Lesson5/File_sistem/1, 2 Text/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _1__2_Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "startup.txt",
+                   str = "";
+
+            Console.WriteLine("Введите строку для записи в файл");
+            str = Console.ReadLine();
+            File.WriteAllText(filename, str);
+            Console.ReadKey();
+
+            /////////////////////////////
+            DateTime time = DateTime.Now;
+            string str_time = time.ToString("HH:mm");
+            File.AppendAllLines(filename, new[] { str_time });
+
+            Console.WriteLine("В файле:" );
+            string file_text = File.ReadAllText(filename);
+            Console.WriteLine(  file_text);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson5/File_sistem/Bin_file/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Bin_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A:  string str = "";
+            Console.WriteLine("Введите набор чисел через пробел");
+            str = Console.ReadLine();
+            string[] arr_str = str.Split(' ');
+            byte[] arr_numb = new byte[arr_str.Length];
+
+            
+            for (int i = 0; i < arr_numb.Length; i++)
+            {
+                try { arr_numb[i] = Convert.ToByte(arr_str[i]); } catch { Console.WriteLine( "Ошибка ввода"); goto A;  };
+            }
+
+            File.WriteAllBytes("Numbers.bin", arr_numb);
+
+            Console.WriteLine("Чтение из файла:");
+
+            byte[] from_file = File.ReadAllBytes("Numbers.bin");
+
+            for (int j = 0; j< from_file.Length; j++)
+            {
+                Console.Write(from_file[j]+" ");
+
+            }
+                Console.ReadKey();
+
+
+
+
+        }
+
+        
+        
+    }
+}
